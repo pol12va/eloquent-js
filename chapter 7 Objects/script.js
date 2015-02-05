a3e0fd826cc7ba506a40a7c0e2bd39b634b57e25
akamornikov <akamornikov@qulix.com>
2015-02-05 22:54:18 +0300

    Chapter 7

---
+++ chapter 7 Objects/script.js
@@ -0,0 +1,22 @@
+function rowHeights(rows) {
+	return rows.map(function(row) {
+		return row.reduce(function(max, cell) {
+			return Math.max(max, cell.minHeight());
+		}, 0);
+	});
+}
+
+function colWidth(rows) {
+	return rows[0].map(function(_, i) {
+		return rows.reduce(function(max, row) {
+			return Math.max(max, row[i].minWidth());
+		}, 0);
+	})
+}
+
+var bracketTest = function() {
+	var res = [] + {};
+	console.log(res);
+}
+
+bracketTest();
